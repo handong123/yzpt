bdd00963862820f326b8ca1a917466a7f71e2a8c
Han Dong <handong238@tasly.com>
2018-10-20 16:57:39 +0800

    增加Swagger2配置文件

--- yz-message/src/main/java/com/tasly/yzpt/controller/PushController.java
+++ yz-message/src/main/java/com/tasly/yzpt/controller/PushController.java
@@ -3,6 +3,10 @@ package com.tasly.yzpt.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.tasly.yzpt.common.entity.MsgPushEntity;
 import com.tasly.yzpt.service.message.AsyncParserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,22 +16,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("")
 @EnableAsync
 @Slf4j
+@Api("有赞消息服务相关的api")
 public class PushController {
 
     private static final int mode = 1; //服务商
 
-    @Autowired
-    private YZMessageService yzMessageService;
 
     @Autowired
     private AsyncParserService asyncParserService;
-
+    @ApiOperation(value = "异步消息", notes = "接收有赞异步消息")
+    @ApiImplicitParam(name = "entity", value = "消息实体", required = true, dataType = "MsgPushEntity")
     @RequestMapping(value = "/message", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     @ResponseBody
     public Object message(@RequestBody MsgPushEntity entity) {
         log.info("异步调用PushController开始" + Thread.currentThread().getId() + "   " + Thread.currentThread().getName());
-
-        yzMessageService.saveMessage(entity);
 
         JSONObject res = new JSONObject();
         res.put("code", 0);
