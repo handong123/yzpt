16398234c8e7f08d59c6c2b8e559ec1c5f82e8cc
Han Dong <handong238@tasly.com>
2018-10-23 16:32:35 +0800

    springboot-Redis

--- yz-common/src/main/java/com/tasly/yzpt/common/token/YZToken.java
+++ yz-common/src/main/java/com/tasly/yzpt/common/token/YZToken.java
@@ -10,11 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -32,7 +32,7 @@ public class YZToken {
     private KeystoneConfig keystoneConfig;
 
     @Autowired
-    private RedisTemplate<Serializable, String> redisTemplate;
+    private RedisTemplate redisTemplate;
 
     @PostConstruct
     public void init() {
@@ -41,34 +41,34 @@ public class YZToken {
 
     }
 
-//    public static String getToken() {
-//        String token = yzToken.redisTemplate.opsForValue().get(ACCESS_TOKEN);
-//        if (token != null) {
-//            return token;
-//        }
-//        YZKeystone yzKeystone = yzToken.keystoneConfig.yzKeystone();
-//        HttpClient httpClient = new DefaultHttpClient();
-//        HttpClient.Params params = HttpClient.Params.custom()
-//                .add("client_id", yzKeystone.getClientId())
-//                .add("client_secret", yzKeystone.getClientSecret())
-//                .add("grant_type", "silent")
-//                .add("kdt_id", yzKeystone.getKdtId())
-//                .setContentType(ContentType.APPLICATION_FORM_URLENCODED).build();
-//        String resp = httpClient.post("https://open.youzan.com/oauth/token", params);
-//        log.info("OAuthToken==" + resp);
-//        if (StringUtils.isBlank(resp) || !resp.contains(ACCESS_TOKEN)) {
-//            throw new RuntimeException(resp);
-//        }
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
-//        try {
-//            OAuthToken oAuthToken = mapper.readValue(resp, OAuthToken.class);
-//            yzToken.redisTemplate.opsForValue().set(ACCESS_TOKEN, oAuthToken.getAccessToken(), oAuthToken.getExpiresIn(), TimeUnit.SECONDS);
-//
-//            return oAuthToken.getAccessToken();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
+    public static String getToken() {
+        String token = (String) yzToken.redisTemplate.opsForValue().get(ACCESS_TOKEN);
+        if (token != null) {
+            return token;
+        }
+        YZKeystone yzKeystone = yzToken.keystoneConfig.yzKeystone();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpClient.Params params = HttpClient.Params.custom()
+                .add("client_id", yzKeystone.getClientId())
+                .add("client_secret", yzKeystone.getClientSecret())
+                .add("grant_type", "silent")
+                .add("kdt_id", yzKeystone.getKdtId())
+                .setContentType(ContentType.APPLICATION_FORM_URLENCODED).build();
+        String resp = httpClient.post("https://open.youzan.com/oauth/token", params);
+        log.info("OAuthToken==" + resp);
+        if (StringUtils.isBlank(resp) || !resp.contains(ACCESS_TOKEN)) {
+            throw new RuntimeException(resp);
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+        try {
+            OAuthToken oAuthToken = mapper.readValue(resp, OAuthToken.class);
+            yzToken.redisTemplate.opsForValue().set(ACCESS_TOKEN, oAuthToken.getAccessToken(), oAuthToken.getExpiresIn(), TimeUnit.SECONDS);
+
+            return oAuthToken.getAccessToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- yz-service/src/main/java/com/tasly/yzpt/service/message/impl/LogisticsOnlineConfirmServiceImpl.java
+++ yz-service/src/main/java/com/tasly/yzpt/service/message/impl/LogisticsOnlineConfirmServiceImpl.java
@@ -21,33 +21,33 @@ public class LogisticsOnlineConfirmServiceImpl implements LogisticsOnlineConfirm
     @Override
     @Transactional
     public void confirm() {
-
-        log.info("");
-
-        YZClient client = new DefaultYZClient(new Token(YZToken.getToken()));
-        YouzanLogisticsOnlineConfirmParams youzanLogisticsOnlineConfirmParams = new YouzanLogisticsOnlineConfirmParams();
-
-        // 交易订单号
-        youzanLogisticsOnlineConfirmParams.setTid(taslyOrder.getOriginalOrderId());
-        // 物流公司编号，可以通过请求 youzan.logistics.express.get 该接口获得
-        youzanLogisticsOnlineConfirmParams.setOutStype(expressService.getExpressByCode(
-                expressCode).getB2B2_code());
-        // 快递单号（具体一个物流公司的真实快递单号）
-        youzanLogisticsOnlineConfirmParams.setOutSid(expressNumber);
-
-        YouzanLogisticsOnlineConfirm youzanLogisticsOnlineConfirm = new YouzanLogisticsOnlineConfirm();
-        youzanLogisticsOnlineConfirm.setAPIParams(youzanLogisticsOnlineConfirmParams);
-        String result = client.execute(youzanLogisticsOnlineConfirm);
-        Response response = JSON.parseObject(result, Response.class);
-        if (response.getErrorResponse() == null) {
-            YouzanLogisticsOnlineConfirmResult confirmResult = JSON.parseObject(response.getResponse().toString(),
-                    YouzanLogisticsOnlineConfirmResult.class);
-            isSuccess = confirmResult.getIsSuccess();
-            LOG.info("----上传 B2B_2 物流信息成功----");
-        } else {
-            LOG.info("----上传 B2B_2 物流信息失败：-----" + response.getErrorResponse());
-        }
-
-        log.info("结束上传 B2B_2 平台物流信息,店铺=" + taslyOrder.getStoreCode());
+//
+//        log.info("");
+//
+//        YZClient client = new DefaultYZClient(new Token(YZToken.getToken()));
+//        YouzanLogisticsOnlineConfirmParams youzanLogisticsOnlineConfirmParams = new YouzanLogisticsOnlineConfirmParams();
+//
+//        // 交易订单号
+//        youzanLogisticsOnlineConfirmParams.setTid(taslyOrder.getOriginalOrderId());
+//        // 物流公司编号，可以通过请求 youzan.logistics.express.get 该接口获得
+//        youzanLogisticsOnlineConfirmParams.setOutStype(expressService.getExpressByCode(
+//                expressCode).getB2B2_code());
+//        // 快递单号（具体一个物流公司的真实快递单号）
+//        youzanLogisticsOnlineConfirmParams.setOutSid(expressNumber);
+//
+//        YouzanLogisticsOnlineConfirm youzanLogisticsOnlineConfirm = new YouzanLogisticsOnlineConfirm();
+//        youzanLogisticsOnlineConfirm.setAPIParams(youzanLogisticsOnlineConfirmParams);
+//        String result = client.execute(youzanLogisticsOnlineConfirm);
+//        Response response = JSON.parseObject(result, Response.class);
+//        if (response.getErrorResponse() == null) {
+//            YouzanLogisticsOnlineConfirmResult confirmResult = JSON.parseObject(response.getResponse().toString(),
+//                    YouzanLogisticsOnlineConfirmResult.class);
+//            isSuccess = confirmResult.getIsSuccess();
+//            LOG.info("----上传 B2B_2 物流信息成功----");
+//        } else {
+//            LOG.info("----上传 B2B_2 物流信息失败：-----" + response.getErrorResponse());
+//        }
+//
+//        log.info("结束上传 B2B_2 平台物流信息,店铺=" + taslyOrder.getStoreCode());
     }
 }
