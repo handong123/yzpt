da41151599b511abda6862dbfd4b6a85c29f5b06
xiashengxin <xiashengxin@tasly.com>
2018-11-01 15:00:42 +0800

    出库取消

--- yz-service/src/main/java/com/tasly/yzpt/service/event/RefundBuyerToWmsListener.java
+++ yz-service/src/main/java/com/tasly/yzpt/service/event/RefundBuyerToWmsListener.java
@@ -1,6 +1,8 @@
 package com.tasly.yzpt.service.event;
 
+import com.tasly.yzpt.service.wms.WmsOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class RefundBuyerToWmsListener {
+
+    @Autowired
+    WmsOrderService wmsOrderService;
     /**
      * 申请退款订单下发WMS
      *
@@ -16,7 +21,6 @@ public class RefundBuyerToWmsListener {
     @EventListener
     @Async
     public void listener(RefundBuyerToWmsEvent event) {
-
-
+        wmsOrderService.sendCancelToWmsOrder(event.getEntity());
     }
 }
